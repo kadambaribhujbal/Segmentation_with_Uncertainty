167360dcb85c06623b55eceabf8b7d776192b1bb
Kadambari Bhujbal <kadambaribhujbal27@gmail.com>
2024-11-24 13:07:03 -0500

    base softmax var none

--- hyperparams.py
+++ hyperparams.py
@@ -13,6 +13,7 @@ def get_hyperparams():
         "n_epoch": 30,
         "model": "FCDenseNet103",
         "mode": "combined",
+        "mode": "base",
         # "mode": "aleatoric",
     }
     return hyperprams
