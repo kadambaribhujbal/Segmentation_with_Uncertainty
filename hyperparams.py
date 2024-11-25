b7a0676dc68e178259ec71d4e2f5c2a05931001f
Kadambari Bhujbal <kadambaribhujbal27@gmail.com>
2024-11-24 21:32:07 -0500

    combine 12 var cross entropy

--- hyperparams.py
+++ hyperparams.py
@@ -12,8 +12,8 @@ def get_hyperparams():
         "decay_per_n_epoch": 1,
         "n_epoch": 30,
         "model": "FCDenseNet103",
-        # "mode": "combined",
-        "mode": "epistemic",
+        "mode": "combined",
+        # "mode": "epistemic",
         # "mode": "base",
         # "mode": "aleatoric",
     }
