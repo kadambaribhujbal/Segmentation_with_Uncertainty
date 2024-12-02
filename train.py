df462eaac5bd839f046115e320f1d472bc54523c
Kadambari Bhujbal <kadambaribhujbal27@gmail.com>
2024-12-02 14:21:21 -0500

    combine 2nd dec 100 plus 30

--- train.py
+++ train.py
@@ -338,7 +338,7 @@ if __name__ == "__main__":
     entropies = []
 
     # Define the checkpoint path inside the code
-    checkpoint_path = "/content/Segmentation_with_Uncertainty/trained_weights/2nddec_combine_0.2_model_epoch_95_val_loss_0.5251.pth"
+    checkpoint_path = "/content/Segmentation_with_Uncertainty/trained_weights/2nddec_combine_0.2_model_epoch_100_val_loss_0.5163.pth"
 
     # Check if a checkpoint exists and load it
     if os.path.exists(checkpoint_path):
